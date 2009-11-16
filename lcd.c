7b85e6a103c24d5e149e3c53d772ecc71bc33038
John Hughes <jondo2010@gmail.com>
2009-11-15 22:19:10 -0600

    Cleanups

--- lcd.c
+++ lcd.c
@@ -1,8 +1,9 @@
 /*
  * lcd.c
+ * Library for the Epson SED1335 LCD Controller
  *
- *  Created on: 2009-11-08
- *      Author: John Hughes
+ * Created on: 2009-11-08
+ * John Hughes <jondo2010@gmail.com>
  */
 
 #include <73x_lib.h>
@@ -40,14 +41,21 @@ void data_out (u8 i) //Data Output 16-bit Bus Interface
 	delay(1);
 }
 
-void lcd_set_scroll (u16 textAddr, u8 textHeight, u16 picAddr, u8 picHeight)
+void
+lcd_set_scroll
+(
+  uint16_t  textAddr,
+  uint8_t   textHeight,
+  uint16_t  picAddr,
+  uint8_t   picHeight
+)
 {
 	comm_out (SCROLL);
-	data_out ((u8) textAddr); // P1: (SAD 1L)
+	data_out ((uint8_t) textAddr); // P1: (SAD 1L)
 	data_out (textAddr >> 8); // P2: (SAD 1H)
 	data_out (textHeight); // P3: (SL 1)
 
-	data_out ((u8) picAddr); // P4: (SAD 2L)
+	data_out ((uint8_t) picAddr); // P4: (SAD 2L)
 	data_out (picAddr >> 8); // P5: (SAD 2H)
 	data_out (picHeight); // P6: (SL 2)
 
@@ -57,26 +65,44 @@ void lcd_set_scroll (u16 textAddr, u8 textHeight, u16 picAddr, u8 picHeight)
 	data_out (0x00); // P10: (SAD 4H)
 }
 
-void lcd_set_hscroll (u8 position)
+void
+lcd_set_hscroll
+(
+  uint8_t position
+)
 {
 	comm_out (HDOT_SCR);
 	data_out (position);
 }
 
-void lcd_set_overlay (u8 overlay)
+void
+lcd_set_overlay
+(
+  uint8_t overlay
+)
 {
 	comm_out (OVLAY); // Set display overlay format
 	data_out (overlay);
 }
 
-void lcd_set_cursor_pointer (u16 addr)
+void 
+lcd_set_cursor_pointer 
+(
+  uint16_t addr
+)
 {
 	comm_out (CSRW);
 	data_out ((u8)addr);
 	data_out (addr >> 8);
 }
 
-void lcd_draw_string (u8 *string, u16 addr, u8 length)
+void
+lcd_draw_string
+(
+  uint8_t *string,
+  uint16_t addr,
+  uint8_t length
+)
 {
 	lcd_set_cursor_pointer (addr);
 	comm_out (MWRITE);
@@ -86,22 +112,24 @@ void lcd_draw_string (u8 *string, u16 addr, u8 length)
 	}
 }
 
-void lcd_clear_screen ()
+void
+lcd_clear_screen ()
 {
-	int n;
+	uint8_t n;
+
 	lcd_set_cursor_pointer (0x0000);
 	comm_out (MWRITE);
-	for (n=0;n<1200;n++){
-		data_out (0x20); // space char
-	}
+	for (n=0;n<1200;n++)
+		data_out (0x20); // Space character
+
 	lcd_set_cursor_pointer (0x04b0);
 	comm_out (MWRITE);
-	for (n=0;n<9600;n++){
+	for (n=0;n<9600;n++)
 		data_out (0x00);
-	}
 }
 
-void lcd_reset ()
+void
+lcd_reset ()
 {
 	GPIO_BitWrite ( GPIO3, LE, Bit_SET );
 
@@ -114,7 +142,12 @@ void lcd_reset ()
 	delay(1000);
 }
 
-void lcd_system_set (u8 cr, u8 tcr)
+void
+lcd_system_set
+(
+  uint8_t cr,
+  uint8_t tcr
+)
 {
 	comm_out (SYSTEM_SET);
 	delay (5);
@@ -129,7 +162,15 @@ void lcd_system_set (u8 cr, u8 tcr)
 	data_out (0x00); // P8: (APH 0..7)
 }
 
-void lcd_set_display_state (u8 on_off, u8 cursor_state, u8 sad1_state, u8 sad2_state, u8 sad3_state)
+void
+lcd_set_display_state
+(
+  uint8_t on_off,
+  uint8_t cursor_state,
+  uint8_t sad1_state,
+  uint8_t sad2_state,
+  uint8_t sad3_state
+)
 {
 	if (on_off)
 		comm_out (DISP_ON);
@@ -144,41 +185,48 @@ void lcd_set_display_state (u8 on_off, u8 cursor_state, u8 sad1_state, u8 sad2_s
 	 */
 	delay(5);
 
-	data_out ( cursor_state |
-				(sad1_state << 2) |
-				(sad2_state << 4) |
-				(sad3_state << 6)
-			 );
+	data_out (
+    cursor_state |
+    (sad1_state << 2) |
+    (sad2_state << 4) |
+    (sad3_state << 6)
+  );
 }
 
-void lcd_set_cursor_form (u8 cursor_width, u8 cursor_height, u8 cursor_type)
+void
+lcd_set_cursor_form
+(
+  uint8_t cursor_width,
+  uint8_t cursor_height,
+  uint8_t cursor_type
+)
 {
 	comm_out (CSRFORM);
 	data_out ((cursor_width - 1) & 0x0f);
 	data_out (cursor_type | ((cursor_height -1) & 0x0f));
 }
 
-void lcd_set_cursor_direction (u8 cursor_direction)
+void
+lcd_set_cursor_direction
+(
+  uint8_t cursor_direction
+)
 {
 	comm_out (CSRDIR | (cursor_direction & 0x03));
 }
 
-void lcd_init()
+void
+lcd_init ()
 {
 	lcd_set_scroll (0x0000, 0xef, 0x04b0, 0xef);
-//delay(5);
 	lcd_set_hscroll (0x00); // Set horizontal scroll position
-//	delay(5);
 	lcd_set_overlay (0x00);
-//	delay(5);
+
 	// Turn off display, set blinking cursor, all display blocks steady on
 	lcd_set_display_state (0, CURSOR_BLINK_2HZ, SAD_STEADY, SAD_STEADY, SAD_STEADY);
-//	delay(5);
 	lcd_set_cursor_form (5, 7, BLOCK_CURSOR); // Set a 4x6 block cursor
-//	delay(5);
 	lcd_set_cursor_direction (CURSOR_RIGHT);
-//	delay(5);
+
 	// Turn display back on, cursor blinking, display blocks 1 and 2 steady, 3 off.
 	lcd_set_display_state (1, CURSOR_BLINK_2HZ, SAD_STEADY, SAD_STEADY, SAD_OFF);
-
 }
